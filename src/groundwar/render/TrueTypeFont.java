a3584e1b09021a5383eb7c76d2d04ec6f96caa42
Lucas Pickering <pickering.l@husky.neu.edu>
2015-12-26 15:36:08 -0500

    Fixed vertical text alignment

--- src/groundwar/render/TrueTypeFont.java
+++ src/groundwar/render/TrueTypeFont.java
@@ -144,15 +144,13 @@ public class TrueTypeFont {
    * @param x          the x location to draw at
    * @param y          the y location to draw at
    * @param color      the color to draw with
-   * @param horizAlign the {@link HorizAlignment} to draw with (non-null)
-   * @param vertAlign  the {@link VertAlignment} to draw with (non-null)
-   * @throws NullPointerException if {@code text == null} or {@code horizAlign == null} or {@code
-   *                              vertAlign == null}
+   * @param horizAlign the {@link HorizAlignment} to draw with
+   * @param vertAlign  the {@link VertAlignment} to draw with
+   * @throws NullPointerException if {@code text == null}
    */
   public void draw(String text, int x, int y, int color,
                    HorizAlignment horizAlign, VertAlignment vertAlign) {
     Objects.requireNonNull(text);
-    Objects.requireNonNull(horizAlign);
     // Set the color (aren't bitshifts cool?)
     GL11.glColor4f((color >> 16 & 0xff) / 255.0f, (color >> 8 & 0xff) / 255.0f,
                    (color & 0xff) / 255.0f, (color >> 24 & 0xff) / 255.0f);
@@ -161,8 +159,20 @@ public class TrueTypeFont {
 
     int xTmp = x;
     int yTmp = y;
+
+    // Adjust y for vertical alignment
+    switch (vertAlign) {
+      case CENTER:
+        yTmp -= charHeight * lines.length / 2;
+        break;
+      case BOTTOM:
+        yTmp -= charHeight * lines.length;
+        break;
+    }
+
     GL11.glBegin(GL11.GL_QUADS);
     for (String line : lines) {
+      // Adjust x for horizontal alignment
       switch (horizAlign) {
         case CENTER:
           xTmp = x - (int) getStringWidth(line) / 2;
@@ -171,14 +181,7 @@ public class TrueTypeFont {
           xTmp = x - (int) getStringWidth(line);
           break;
       }
-      switch (vertAlign) {
-        case CENTER:
-          yTmp -= charHeight / 2;
-          break;
-        case BOTTOM:
-          yTmp -= charHeight;
-          break;
-      }
+
       for (char c : line.toCharArray()) {
         final float width = getCharWidth(c);
 
